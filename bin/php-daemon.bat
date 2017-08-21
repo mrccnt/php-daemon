3ea7379c4018d9548483ae53799eda0dba1d5162
Marco <marco.conti@crealogix.com>
2017-08-22 00:17:14 +0200

    Startup

--- bin/php-daemon.bat
+++ bin/php-daemon.bat
@@ -3,23 +3,19 @@
 if "%OS%"=="Windows_NT" @setlocal
 
 rem %~dp0 is expanded pathname of the current script under NT
-set DEFAULT_CLIENT_MIGRATION_HOME=%~dp0..
+set CURR_HOME=%~dp0
 
 goto init
 goto cleanup
 
 :init
-
-if "%CLIENT_MIGRATION_HOME%" == "" set CLIENT_MIGRATION_HOME=%DEFAULT_CLIENT_MIGRATION_HOME%
-set DEFAULT_CLIENT_MIGRATION_HOME=
-
 if "%PHP_COMMAND%" == "" goto no_phpcommand
 
 goto run
 goto cleanup
 
 :run
-"%PHP_COMMAND%" -d phar.readonly=off -d html_errors=off -qC "%CLIENT_MIGRATION_HOME%\bin\php-daemon.php" %*
+"%PHP_COMMAND%" -qC "%CURR_HOME%\bin\php-daemon" %*
 goto cleanup
 
 :no_phpcommand
@@ -28,11 +24,6 @@ REM echo WARNING: Set environment var PHP_COMMAND to the location of your php.ex
 REM echo          executable (e.g. C:\PHP\php.exe).  (Assuming php.exe on Path)
 REM echo ------------------------------------------------------------------------
 set PHP_COMMAND=php.exe
-goto init
-
-:err_home
-echo ERROR: Environment var CLIENT_MIGRATION_HOME not set. Please point this
-echo variable to your local php-daemon installation!
 goto cleanup
 
 :cleanup

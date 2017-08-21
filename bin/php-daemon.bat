81bae2106a02e55f4c6001fde27df1f81e547572
Marco <marco.conti@crealogix.com>
2017-08-22 00:18:35 +0200

    Startup

--- bin/php-daemon.bat
+++ bin/php-daemon.bat
@@ -2,30 +2,8 @@
 
 if "%OS%"=="Windows_NT" @setlocal
 
-rem %~dp0 is expanded pathname of the current script under NT
 set CURR_HOME=%~dp0
 
-goto init
-goto cleanup
-
-:init
-if "%PHP_COMMAND%" == "" goto no_phpcommand
-
-goto run
-goto cleanup
-
-:run
 "%PHP_COMMAND%" -qC "%CURR_HOME%\bin\php-daemon" %*
-goto cleanup
 
-:no_phpcommand
-REM echo ------------------------------------------------------------------------
-REM echo WARNING: Set environment var PHP_COMMAND to the location of your php.exe
-REM echo          executable (e.g. C:\PHP\php.exe).  (Assuming php.exe on Path)
-REM echo ------------------------------------------------------------------------
-set PHP_COMMAND=php.exe
-goto cleanup
-
-:cleanup
 if "%OS%"=="Windows_NT" @endlocal
-REM pause

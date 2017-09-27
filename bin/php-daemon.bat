de576a2bc4d345f4d1c4b9cf6380db30f459a312
Marco <marco.conti@crealogix.com>
2017-09-28 00:56:51 +0200

    Check binary

--- bin/php-daemon.bat
+++ bin/php-daemon.bat
@@ -1,9 +1,15 @@
 @echo off
 
+where php.exe >nul 2>nul
+if %errorlevel%==1 (
+    @echo php not found in path
+    exit
+)
+
 if "%OS%"=="Windows_NT" @setlocal
 
 set CURR_HOME=%~dp0
 
-"%PHP_COMMAND%" -qC "%CURR_HOME%\bin\php-daemon" %*
+php -qC "%CURR_HOME%\bin\php-daemon" %*
 
 if "%OS%"=="Windows_NT" @endlocal
